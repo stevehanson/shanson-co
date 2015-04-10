4398f8cc0e75e2b2e5444c6937f135445adee874
Steve Hanson <s.hanson5@gmail.com>
2015-04-09 20:00:14 -0500

    Fix markdown render after upgrading ember

--- app/assets/javascripts/admin/helpers.js
+++ app/assets/javascripts/admin/helpers.js
@@ -1,6 +1,5 @@
 Ember.Handlebars.registerBoundHelper('markdown', function (content) {
-
   if(content && content.length > 0) {
-    return new Handlebars.SafeString(new Showdown.converter().makeHtml(content));
+    return new Ember.Handlebars.SafeString(new Showdown.converter().makeHtml(content));
   }
 });
