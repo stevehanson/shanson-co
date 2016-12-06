fc00c17c29455b68af3419c80dbc27aaf2256c7b
Steve Hanson <s.hanson5@gmail.com>
2016-12-06 13:27:02 -0600

    Add blog engine

--- Gemfile
+++ Gemfile
@@ -9,6 +9,12 @@ gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
 # Middleman Gems
-gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-autoprefixer'
+gem "middleman", "~> 4.1"
+gem "middleman-blog"
+
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -16,33 +16,68 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
+###
+# Helpers
+###
+
+helpers do
+  def human_date(date)
+    date.strftime("%b %d, %Y")
+  end
+
+  # in minutes
+  def read_length(content)
+    words_per_minute = 200.0
+    num_words = content.split.size
+    (num_words / words_per_minute).ceil
+  end
+end
 
 activate :autoprefixer
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-helpers do
-  def human_date(dt)
-    "12/12/2012"
-  end
-
-  def read_length(txt)
-    "2 minutes"
-  end
-
-  def markdown(txt)
-    "markdown"
-  end
-end
 
 # Build-specific configuration
 configure :build do
