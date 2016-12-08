fc7a927f6ab19a7eab091ca294c481333f146132
Steve Hanson <s.hanson5@gmail.com>
2016-12-08 16:37:36 -0600

    Update kudos script to update with transaction to handle the likely case that millions of people are kudoing my posts at the same

--- source/javascripts/kudos.js
+++ source/javascripts/kudos.js
@@ -28,9 +28,17 @@
   };
 
   Kudo.prototype.loadKudos = function() {
-    firebase.database().ref('/posts/' + this.postId + '/kudos_count').once('value').then(function(snapshot) {
-      this.kudosCount = parseInt(snapshot.val());
-      this.updateKudoCount(this.kudosCount);
+    this.postRef = firebase.database().ref('/posts/' + this.postId);
+
+    // will listen for updates
+    this.postRef.on('value', function(snapshot) {
+      if(!snapshot.val()) {
+        this.postRef.set({ kudos_count: 0 });
+      }
+
+      this.postData = snapshot.val() || { kudos_count: 0 };
+      this.kudosCount = parseInt(this.postData.kudos_count);
+      this.updateKudoCountDisplay(this.kudosCount);
       _(this.element).fade('in', 100);
     }.bind(this));
   };
@@ -39,41 +47,54 @@
     var kudos = getKudos();
     kudos[this.postId] = true;
     localStorage.setItem('kudos', JSON.stringify(kudos));
+    this.toggleKudo('add');
 
-    var self = this;
-    this.kudosCount += 1;
-
-    // TODO: actually sync kudos in a transaction and make sure we are setting the right value
-    firebase.database().ref('posts/' + this.postId + '/kudos_count').set(self.kudosCount);
-
-    self.displayKudoThanks(self.kudosCount);
+    this.displayKudoThanks();
     this.isKudoed = true;
     this.element.classList.add('kudoed');
     this.track('add');
+  };
+
+  Kudo.prototype.toggleKudo = function(operation) {
+    var self = this;
+
+    // perform kudo update in a transaction. if the count
+    // has been updated since the user was on the page, then
+    // it will fail and retry until it succeeds
+    this.postRef.transaction(function(post) {
+      if (post) {
+        if (operation === 'add') {
+          post.kudos_count = post.kudos_count || 0;
+          post.kudos_count++;
+        } else {
+          post.kudos_count = post.kudos_count || 1;
+          post.kudos_count--;
+        }
+
+        self.kudosCount = post.kudos_count;
+        self.updateKudoCountDisplay(self.kudosCount);
+      }
+
+      return post;
+    });
   };
 
   Kudo.prototype.removeKudo = function() {
     var kudos = getKudos();
     delete kudos[this.postId];
     localStorage.setItem('kudos', JSON.stringify(kudos));
+    this.toggleKudo('remove');
 
-    var self = this;
-    this.kudosCount -= 1;
-
-    // TODO: actually sync kudos in a transaction and make sure we are setting the right value
-    firebase.database().ref('posts/' + this.postId + '/kudos_count').set(self.kudosCount);
-
-    self.updateKudoCount(self.kudosCount);
+    this.updateKudoCountDisplay(this.kudosCount);
     this.isKudoed = false;
     this.element.classList.remove('kudoed');
     this.track('remove');
   };
 
-  Kudo.prototype.displayKudoThanks = function(kudoCount) {
+  Kudo.prototype.displayKudoThanks = function() {
     var self = this;
     this.element.querySelector('.kudo-count-message').style.display = 'none'; // hide count
     this.element.querySelector('.kudo-thanks').style.display = 'block';  // display thanks
-    this.updateKudoCount(kudoCount);
 
     setTimeout(function() {
       _(self.element.querySelector('.kudo-thanks')).fade('out', 100);         // fade thanks out after 1.5s
@@ -84,7 +105,7 @@
     // }
   };
 
-  Kudo.prototype.updateKudoCount = function(count) {
+  Kudo.prototype.updateKudoCountDisplay = function(count) {
     this.element.querySelector('.kudo-count').textContent = count;
     var kudoWord = count === 1 ? 'kudo' : 'kudos';
     this.element.querySelector('.kudo-word').textContent = kudoWord;
