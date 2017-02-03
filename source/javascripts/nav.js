65c94ce9b979c17061eb68169efc3e9151df5d81
Steve Hanson <s.hanson5@gmail.com>
2017-02-03 17:42:14 -0600

    Style changes

--- source/javascripts/nav.js
+++ source/javascripts/nav.js
@@ -3,7 +3,14 @@
 
   if(toggle) {
     toggle.addEventListener('click', function() {
-      document.getElementById('nav').classList.toggle('open');
+      var nav = document.getElementById('nav');
+      nav.classList.toggle('open');
+
+      // temporarily add 'closed' class so transition applies
+      if(!nav.classList.contains('open')) {
+        nav.classList.add('closed');
+        setTimeout(function() { nav.classList.remove('closed'); }, 1000);
+      }
     });
   }
 })(document);
